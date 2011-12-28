8af320ae23522ddc7f8343dfdc2b3bafda68aa67
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-12-27 18:03:09 -0800

    Use XHR to load the data for the benchmarks suite.
    While at that, the Run button to trigger the benchmarks is also added.
    
    An external library, https://github.com/ilinsky/xmlhttprequest, is used
    for cross-browser XML Http Request object.
    
    http://code.google.com/p/esprima/issues/detail?id=39
    http://code.google.com/p/esprima/issues/detail?id=23

--- test/benchmarks.js
+++ test/benchmarks.js
@@ -1,7 +1,9 @@
 /*jslint browser: true */
+/*global runAllBenchmark: true */
 
 function runBenchmarks() {
     'use strict';
+
     var index = 0,
         totalSize = 0,
         totalTime = 0,
@@ -18,6 +20,14 @@ function runBenchmarks() {
         'mootools-1.3.2'
     ];
 
+    function id(i) {
+        return document.getElementById(i);
+    }
+
+    function kb(bytes) {
+        return (bytes / 1024).toFixed(1);
+    }
+
     function setText(el, str) {
         if (typeof el.innerText === 'string') {
             el.innerText = str;
@@ -26,49 +36,101 @@ function runBenchmarks() {
         }
     }
 
-    function showVersion() {
-        var el = document.getElementById('benchmarkjs-version');
-        setText(el, ' version ' + window.Benchmark.version);
-        el = document.getElementById('version');
-        setText(el, window.esprima.version);
-    }
-
-    function showStatus(name) {
-        var el = document.getElementById(name + '-time');
-        setText(el, 'Running...');
-    }
-
-    function finish() {
-        var el = document.getElementById('status');
-        setText(el, 'Completed.');
-        el = document.getElementById('total-size');
-        setText(el, (totalSize / 1024).toFixed(1));
-        el = document.getElementById('total-time');
-        setText(el, (1000 * totalTime).toFixed(1));
-    }
-
     function showResult(name, size, stats) {
         var el;
-        el = document.getElementById(name + '-size');
-        setText(el, (size / 1024).toFixed(1));
-        el = document.getElementById(name + '-time');
+        el = id(name + '-time');
         setText(el, (1000 * stats.mean).toFixed(1));
-        el = document.getElementById(name + '-variance');
+        el = id(name + '-variance');
         setText(el, (1000 * stats.variance).toFixed(1));
+    }
+
+    function ready() {
+        setText(id('status'), 'Ready.');
+        id('run').disabled = false;
+        id('run').style.visibility = 'visible';
+    }
+
+    function load(tst, callback) {
+        var el = id('status'),
+            xhr = new XMLHttpRequest(),
+            src = '3rdparty/' + tst + '.js';
+
+        try {
+            xhr.timeout = 30000;
+            xhr.open('GET', src, true);
+            setText(el, 'Please wait. Loading ' + src);
+
+            xhr.ontimeout = function () {
+                setText(el, 'Please wait. Error: time out while loading ' + src + ' ');
+                callback.apply();
+            };
+
+            xhr.onreadystatechange = function () {
+                var success = false,
+                    size = 0;
+
+                if (this.readyState === XMLHttpRequest.DONE) {
+                    if (this.status === 200) {
+                        window.data = window.data || {};
+                        window.data[tst] = this.responseText;
+                        size = this.responseText.length;
+                        totalSize += size;
+                        success = true;
+                    }
+                }
+
+                if (success) {
+                    setText(id(tst + '-size'), kb(size));
+                } else {
+                    setText(el, 'Please wait. Error loading ' + src);
+                    setText(id(tst + '-size'), 'Error');
+                }
+
+                callback.apply();
+            };
+
+            xhr.send(null);
+        } catch (e) {
+            setText(el, 'Please wait. Error loading ' + src);
+            callback.apply();
+        }
+    }
+
+    function loadTests() {
+        var sources = fixture.slice();
+
+        function loadNextTest() {
+            var tst;
+
+            if (sources.length > 0) {
+                tst = sources[0];
+                sources.splice(0, 1);
+                window.setTimeout(function () {
+                    load(tst, loadNextTest);
+                }, 100);
+            } else {
+                setText(id('total-size'), kb(totalSize));
+                ready();
+            }
+        }
+
+        id('run').style.visibility = 'hidden';
+        loadNextTest();
     }
 
     function runBenchmark() {
         var el, test, source, benchmark;
 
         if (index >= fixture.length) {
-            finish();
+            setText(id('total-time'), (1000 * totalTime).toFixed(1));
+            ready();
             return;
         }
 
         test = fixture[index];
-        el = document.getElementById(test);
-        source = (typeof el.innerText === 'string') ? el.innerText : el.textContent;
-        showStatus(test);
+        el = id(test);
+        source = window.data[test];
+        setText(id(test + '-time'), 'Running...');
 
         // Force the result to be held in this array, thus defeating any
         // possible "dead core elimination" optimization.
@@ -80,7 +142,6 @@ function runBenchmarks() {
         }, {
             'onComplete': function () {
                 showResult(this.name, source.length, this.stats);
-                totalSize += source.length;
                 totalTime += this.stats.mean;
             }
         });
@@ -92,7 +153,25 @@ function runBenchmarks() {
         }, 211);
     }
 
-    showVersion();
-    window.setTimeout(runBenchmark, 211);
+    id('run').onclick = function () {
+
+        for (index = 0; index < fixture.length; index += 1) {
+            setText(id(fixture[index] + '-time'), '');
+            setText(id(fixture[index] + '-variance'), '');
+        }
+        setText(id('total-time'), '');
+
+        setText(id('status'), 'Please wait. Running benchmarks...');
+        id('run').style.visibility = 'hidden';
+
+        index = 0;
+        totalTime = 0;
+        runBenchmark();
+    };
+
+    setText(id('benchmarkjs-version'), ' version ' + window.Benchmark.version);
+    setText(id('version'), window.esprima.version);
+
+    loadTests();
 }
 
