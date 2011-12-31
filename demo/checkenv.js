844423d0e8474ddebb736df9e8085c5a0266a3f7
Ariya Hidayat <ariya.hidayat@gmail.com>
2011-12-31 01:05:01 -0800

    Use platform detection library.
    Instead of rolling out another code, just reuse platform detection
    library from https://github.com/bestiejs/platform.js.
    
    http://code.google.com/p/esprima/issues/detail?id=95

--- demo/checkenv.js
+++ demo/checkenv.js
@@ -2,30 +2,11 @@
 /*jslint browser:true */
 (function (window) {
     'use strict';
-    var platform,
-        ua,
-        isMac,
-        isWindows,
-        isChrome,
-        isSafari,
-        version,
-        majorVersion;
 
-    platform = window.navigator.platform;
-    ua = window.navigator.userAgent;
-
-    isMac = (platform === 'MacIntel') || (platform === 'MacPPC');
-    isWindows = (platform === 'Win32');
-    isChrome = ua.match(/ Chrome\//) !== null;
-    isSafari = !isChrome && (ua.match(/Safari\//) !== null);
-
-    version = ua.match(/ Version\/([0-9]+)/);
-    if (version && version.length > 1) {
-        majorVersion = parseInt(version[1], 10);
-    }
+    var majorVersion = parseInt(window.platform.version.split('.')[0], 10);
 
     window.checkEnv = function () {
-        if (isSafari && (isMac || isWindows) && (majorVersion <= 3)) {
+        if (window.platform.name === 'Safari' && majorVersion <= 3) {
             throw new Error('CodeMirror does not support Safari <= 3');
         }
     };
