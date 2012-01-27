ed26250c597affa32c79d02d73a0d3f8f66daaaa
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-01-26 22:57:10 -0800

    Use line and column location info in the regex collector demo.
    http://code.google.com/p/esprima/issues/detail?id=6
    http://code.google.com/p/esprima/issues/detail?id=104

--- demo/collector.js
+++ demo/collector.js
@@ -56,66 +56,13 @@ function collectRegex() {
         }
 
         timerId = window.setTimeout(function () {
-            var code, lookup, options, result, i, str;
+            var code, result, i, str;
 
             if (typeof window.editor === 'undefined') {
                 code = document.getElementById('code').value;
             } else {
                 code = window.editor.getValue();
             }
-
-            // While waiting for (line, column) support in Esprima
-            // (see http://code.google.com/p/esprima/issues/detail?id=6).
-            lookup = (function (str) {
-                var i, len, ch, table;
-
-                table = [];
-                if (typeof str === 'string') {
-                    len = str.length;
-                    for (i = 0; i < len; i += 1) {
-                        if (isLineTerminator(str.charAt(i))) {
-                            table.push(i);
-                        }
-                    }
-                    table.push(len);
-                }
-
-                function binarySearch(pos, p, q) {
-                    var middle = Math.floor((p + q) / 2),
-                        value = table[middle];
-
-                    if (p >= q) {
-                        if (pos < value) {
-                            return {
-                                line: p,
-                                column: (p > 0) ? (pos - table[p - 1] - 1) : pos
-                            };
-                        } else {
-                            return {
-                                line: p + 1,
-                                column: pos - table[p]
-                            };
-                        }
-                    }
-                    if (pos < value) {
-                        return binarySearch(pos, p, Math.max(p, middle - 1));
-                    } else {
-                        return binarySearch(pos, Math.min(middle + 1, q), q);
-                    }
-                }
-
-                return function (pos) {
-                    var loc = binarySearch(pos, 0, table.length - 1);
-                    return 'Line ' + (loc.line + 1) + ' column ' + (loc.column + 1);
-                };
-            }(code));
-            window.lookupFunction = lookup;
-
-            options = {
-                range: true
-            };
-
-            result = [];
 
             // Executes f on the object and its children (recursively).
             function visit(object, f) {
@@ -151,7 +98,12 @@ function collectRegex() {
                     if (node.value instanceof RegExp) {
                         str = node.value.toString();
                         if (str[0] === '/') {
-                            result.push(node);
+                            result.push({
+                                type: 'Literal',
+                                value: node.value,
+                                line: node.loc.start.line,
+                                column: node.loc.start.column
+                            });
                         }
                     }
                 }
@@ -165,7 +117,8 @@ function collectRegex() {
                                     result.push({
                                         type: 'Literal',
                                         value: value,
-                                        range: node.range
+                                        line: node.loc.start.line,
+                                        column: node.loc.start.column
                                     });
                                 }
                             }
@@ -177,7 +130,8 @@ function collectRegex() {
                                     result.push({
                                         type: 'Literal',
                                         value: value,
-                                        range: node.range
+                                        line: node.loc.start.line,
+                                        column: node.loc.start.column
                                     });
                                 }
                             }
@@ -187,13 +141,15 @@ function collectRegex() {
             }
 
             try {
-                visit(window.esprima.parse(code, options), collect);
+                result = [];
+                visit(window.esprima.parse(code, { loc: true }), collect);
 
                 if (result.length > 0) {
                     str = '<p>Found <b>' + result.length + '</b> regex(s):</p>';
                     for (i = 0; i < result.length; i += 1) {
-                        str += '<p>' + lookup(result[i].range[0]) + ': <code>';
-                        str += escaped(result[i].value.toString()) + '</code>';
+                        str += '<p>' + 'Line ' + result[i].line;
+                        str += ' column ' + (1 + result[i].column);
+                        str += ': <code>'; str += escaped(result[i].value.toString()) + '</code>';
                         str += '</p>';
                     }
                     id('result').innerHTML = str;
