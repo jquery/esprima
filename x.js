01fb0f7489cabb0ca6c66eba2a7ad8434430106d
Wolfgang Kluge <wolfgang@klugesoftware.de>
2012-02-28 01:41:44 +0100

    generate() fails on consecutive unary expressions
    The generated output of expressions like `-(-1)` is `--1`.
    This is a fix for it.
    
    I tried to change generateExpression() in a way that no
    superfluous parenthesis are generated.
    So `+(-1)` becomes `+-1` and `typeof typeof 1` remains constant.
    
    http://code.google.com/p/esprima/issues/detail?id=198

---
+++ x.js
@@ -0,0 +1,8 @@
+console.log(typeof typeof 1);
+console.log(+-1);
+console.log(+(-1));
+console.log(-(+(-1)));
+console.log(-+-1);
+console.log(1 + + + 1);
+console.log(1+(+(+(1))));
+console.log(1+ +(+(1)));
