596cc3fe8defd7bb0018d7175e2d72cd38483a81
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-02-28 19:13:07 -0800

    Faster isKeyword() implementation.
    Latest V8 shows at least 5% improvement on the benchmarks suite.
    
    Based on the work from Wolfgang Kluge <wolfgang@klugesoftware.de>.
    
    http://code.google.com/p/esprima/issues/detail?id=199

--- esprima.js
+++ esprima.js
@@ -306,37 +306,39 @@ parseStatement: true, parseSourceElement: true */
     // 7.6.1.1 Keywords
 
     function isKeyword(id) {
-        switch (id) {
+        var keyword = false;
+        switch (id.length) {
+            case 2:
+                keyword = (id === 'if') || (id === 'in') || (id === 'do');
+                break;
+            case 3:
+                keyword = (id === 'var') || (id === 'for') || (id === 'new') || (id === 'try');
+                break;
+            case 4:
+                keyword = (id === 'this') || (id === 'else') || (id === 'case') || (id === 'void') || (id === 'with');
+                break;
+            case 5:
+                keyword = (id === 'while') || (id === 'break') || (id === 'catch') || (id === 'throw');
+                break;
+            case 6:
+                keyword = (id === 'return') || (id === 'typeof') || (id === 'delete') || (id === 'switch');
+                break;
+            case 7:
+                keyword = (id === 'default') || (id === 'finally');
+                break;
+            case 8:
+                keyword = (id === 'function') || (id === 'continue') || (id === 'debugger');
+                break;
+            case 10:
+                keyword = (id === 'instanceof');
+                break;
+        }
 
-        // Keywords.
-        case 'break':
-        case 'case':
-        case 'catch':
-        case 'continue':
-        case 'debugger':
-        case 'default':
-        case 'delete':
-        case 'do':
-        case 'else':
-        case 'finally':
-        case 'for':
-        case 'function':
-        case 'if':
-        case 'in':
-        case 'instanceof':
-        case 'new':
-        case 'return':
-        case 'switch':
-        case 'this':
-        case 'throw':
-        case 'try':
-        case 'typeof':
-        case 'var':
-        case 'void':
-        case 'while':
-        case 'with':
+        if (keyword) {
             return true;
+        }
 
+        switch (id) {
         // Future reserved words.
         // 'const' is specialized as Keyword in V8.
         case 'const':
