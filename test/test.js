745ed0245cd4c81a0a54a6407aac65f4adb66d7b
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-03-06 22:00:01 -0800

    Add a test case for semicolon after a comment.
    This ensures that the following block in consumeSemicolon() function is
    covered:
    
        if (match(';')) {
            lex();
            return;
        }
    
    http://code.google.com/p/esprima/issues/detail?id=221

--- test/test.js
+++ test/test.js
@@ -13627,6 +13627,34 @@ data = {
             }
         },
 
+        'var x /* comment */;': {
+            type: 'VariableDeclaration',
+            declarations: [{
+                type: 'VariableDeclarator',
+                id: {
+                    type: 'Identifier',
+                    name: 'x',
+                    range: [4, 4],
+                    loc: {
+                        start: { line: 1, column: 4 },
+                        end: { line: 1, column: 5 }
+                    }
+                },
+                init: null,
+                range: [4, 4],
+                loc: {
+                    start: { line: 1, column: 4 },
+                    end: { line: 1, column: 5 }
+                }
+            }],
+            kind: 'var',
+            range: [0, 19],
+            loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 1, column: 20 }
+            }
+        },
+
         '{ var x = 14, y = 3\nz; }': {
             type: 'BlockStatement',
             body: [{
