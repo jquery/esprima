194c81de160e3af13daf8f67b6ad95646062fa9f
voloko <voloko@gmail.com>
2012-06-12 15:15:25 -0700

    Fix invalid start location for CallStatement
    CallStatement was returning a position of '(' as a start location. It should return the position of the actual beginning of the statement instead. Example
    
      parse("universe(42)")
    
    was returning
      {
        type: 'CallExpression',
        ...
        range: [0, 12]
      }
    
    while it should
      {
        type: 'CallExpression',
        ...
        range: [8, 12]
      }
    
    Update the wrapper function and fix tests.
    
    http://code.google.com/p/esprima/issues/detail?id=278
    
    Conflicts:
    
    	test/test.js

--- esprima.js
+++ esprima.js
@@ -4148,6 +4148,15 @@ parseYieldExpression: true
                             node.loc.start = node.object.loc.start;
                         }
                     }
+
+                    if (node.type === Syntax.CallExpression) {
+                        if (typeof node.callee.range !== 'undefined') {
+                            node.range[0] = node.callee.range[0];
+                        }
+                        if (typeof node.callee.loc !== 'undefined') {
+                            node.loc.start = node.callee.loc.start;
+                        }
+                    }
                     return node;
                 }
             };
