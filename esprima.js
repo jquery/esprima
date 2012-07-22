18101a485fbfba8b69c639f3eae8c5fd763a18a6
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-07-21 22:19:02 -0700

    Supports AMD.
    As recommended by James Burke, using UMD pattern
    (https://github.com/umdjs/umd) permits using Esprima also with an AMD
    loader such as RequireJS.
    
    A new manual test has been added to verify that this AMD loading works.
    
    http://code.google.com/p/esprima/issues/detail?id=291

--- esprima.js
+++ esprima.js
@@ -29,7 +29,7 @@
 */
 
 /*jslint bitwise:true plusplus:true */
-/*global esprima:true, exports:true,
+/*global esprima:true, define:true, exports:true, window: true,
 throwError: true, createLiteral: true, generateStatement: true,
 parseAssignmentExpression: true, parseBlock: true, parseExpression: true,
 parseFunctionDeclaration: true, parseFunctionExpression: true,
@@ -37,7 +37,19 @@ parseFunctionSourceElements: true, parseVariableIdentifier: true,
 parseLeftHandSideExpression: true,
 parseStatement: true, parseSourceElement: true */
 
-(function (exports) {
+(function (factory) {
+    'use strict';
+
+    // Universal Module Definition (UMD) to support AMD, CommonJS/Node.js,
+    // and plain browser loading,
+    if (typeof define === 'function' && define.amd) {
+        define(['exports'], factory);
+    } else if (typeof exports !== 'undefined') {
+        factory(exports);
+    } else {
+        factory((window.esprima = {}));
+    }
+}(function (exports) {
     'use strict';
 
     var Token,
@@ -3620,5 +3632,5 @@ parseStatement: true, parseSourceElement: true */
         return types;
     }());
 
-}(typeof exports === 'undefined' ? (esprima = {}) : exports));
+}));
 /* vim: set sw=4 ts=4 et tw=80 : */
