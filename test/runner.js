fb8903bc13118668bfbe9113a3e232d9bfc5fcb4
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-09-18 20:08:30 -0700

    Run all the harmony tests.
    http://code.google.com/p/esprima/issues/detail?id=333
    http://code.google.com/p/esprima/issues/detail?id=341

--- test/runner.js
+++ test/runner.js
@@ -321,6 +321,7 @@ if (typeof window !== 'undefined') {
             header;
 
         vm.runInThisContext(fs.readFileSync(__dirname + '/test.js', 'utf-8'));
+        vm.runInThisContext(fs.readFileSync(__dirname + '/harmonytest.js', 'utf-8'));
 
         Object.keys(testFixture).forEach(function (category) {
             Object.keys(testFixture[category]).forEach(function (source) {
