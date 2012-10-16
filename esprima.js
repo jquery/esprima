d7e94689a0704b0ffdead035c78954a67ae8aaf1
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-10-15 22:05:47 -0700

    Add support for Rhino.
    http://code.google.com/p/esprima/issues/detail?id=358

--- esprima.js
+++ esprima.js
@@ -43,19 +43,19 @@ parseStatement: true, parseSourceElement: true, parseModuleBlock: true, parseCon
 parseYieldExpression: true
 */
 
-(function (factory) {
+(function (root, factory) {
     'use strict';
 
     // Universal Module Definition (UMD) to support AMD, CommonJS/Node.js,
-    // and plain browser loading,
+    // Rhino, and plain browser loading.
     if (typeof define === 'function' && define.amd) {
         define(['exports'], factory);
     } else if (typeof exports !== 'undefined') {
         factory(exports);
     } else {
-        factory((window.esprima = {}));
+        factory((root.esprima = {}));
     }
-}(function (exports) {
+}(this, function (exports) {
     'use strict';
 
     var Token,
