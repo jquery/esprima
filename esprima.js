c1c811d46f98b4f35339998418074b2edeb82f51
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-10-16 20:36:55 -0700

    Get ready for non-development version of 1.0.0.

--- esprima.js
+++ esprima.js
@@ -3868,7 +3868,7 @@ parseStatement: true, parseSourceElement: true */
     }
 
     // Sync with package.json.
-    exports.version = '1.0.0-dev';
+    exports.version = '1.0.0';
 
     exports.parse = parse;
 
