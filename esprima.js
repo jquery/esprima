539ae95b217258bfc14f140fc467059717e19b74
Ariya Hidayat <ariya.hidayat@gmail.com>
2012-10-28 02:38:45 -0700

    Simplify scanPunctuator function.
    Catch all single-character punctuators as soon as possible. Also,
    compound assignment can be handled more efficiently.
    
    V8 3.13.7 shows 3% speed-up.
    
    http://code.google.com/p/esprima/issues/detail?id=370
    
    Conflicts:
    
    	esprima.js

--- esprima.js
+++ esprima.js
@@ -599,36 +599,13 @@ parseYieldExpression: true
 
         // Check for most common single-character punctuators.
 
-        if (ch1 === ';' || ch1 === '{' || ch1 === '}') {
+        if (ch1 === '(' || ch1 === ')' || ch1 === ';' || ch1 === ',' ||
+                ch1 === '{' || ch1 === '}' || ch1 === '[' || ch1 === ']' ||
+                ch1 === ':' || ch1 === '?' || ch1 === '~') {
             ++index;
             return {
                 type: Token.Punctuator,
                 value: ch1,
-                lineNumber: lineNumber,
-                lineStart: lineStart,
-                range: [start, index]
-            };
-        }
-
-        if (ch1 === ',' || ch1 === '(' || ch1 === ')') {
-            ++index;
-            return {
-                type: Token.Punctuator,
-                value: ch1,
-                lineNumber: lineNumber,
-                lineStart: lineStart,
-                range: [start, index]
-            };
-        }
-
-        // Dot (.) can also start a floating-point number and ellipsis, hence
-        // the need to check the next character.
-
-        ch2 = source[index + 1];
-        if (ch1 === '.' && !isDecimalDigit(ch2) && ch2 !== '.') {
-            return {
-                type: Token.Punctuator,
-                value: source[index++],
                 lineNumber: lineNumber,
                 lineStart: lineStart,
                 range: [start, index]
@@ -637,6 +614,7 @@ parseYieldExpression: true
 
         // Peek more characters.
 
+        ch2 = source[index + 1];
         ch3 = source[index + 2];
         ch4 = source[index + 3];
 
@@ -655,7 +633,7 @@ parseYieldExpression: true
             }
         }
 
-        // 3-character punctuators: === !== >>> <<= >>= ...
+        // 3-character punctuators: === !== >>> <<= >>=
 
         if (ch1 === '=' && ch2 === '=' && ch3 === '=') {
             index += 3;
@@ -726,19 +704,6 @@ parseYieldExpression: true
         // 2-character punctuators: <= >= == != ++ -- << >> && ||
         // += -= *= %= &= |= ^= /=
 
-        if (ch2 === '=') {
-            if ('<>=!+-*%&|^/'.indexOf(ch1) >= 0) {
-                index += 2;
-                return {
-                    type: Token.Punctuator,
-                    value: ch1 + ch2,
-                    lineNumber: lineNumber,
-                    lineStart: lineStart,
-                    range: [start, index]
-                };
-            }
-        }
-
         if (ch1 === ch2 && ('+-<>&|'.indexOf(ch1) >= 0)) {
             if ('+-<>&|'.indexOf(ch2) >= 0) {
                 index += 2;
@@ -763,12 +728,26 @@ parseYieldExpression: true
             };
         }
 
-        // The remaining 1-character punctuators.
-
-        if ('[]<>+-*%&|^!~?:=/'.indexOf(ch1) >= 0) {
+        if ('<>=!+-*%&|^/'.indexOf(ch1) >= 0) {
+            ++index;
+            if (ch2 === '=') {
+                ++index;
+                ch1 += ch2;
+            }
             return {
                 type: Token.Punctuator,
-                value: source[index++],
+                value: ch1,
+                lineNumber: lineNumber,
+                lineStart: lineStart,
+                range: [start, index]
+            };
+        }
+
+        if (ch1 === '.') {
+            ++index;
+            return {
+                type: Token.Punctuator,
+                value: ch1,
                 lineNumber: lineNumber,
                 lineStart: lineStart,
                 range: [start, index]
@@ -1333,6 +1312,8 @@ parseYieldExpression: true
         if (isIdentifierStart(ch)) {
             return scanIdentifier();
         } else if (ch === '.') {
+            // Dot (.) can also start a floating-point number, hence the need
+            // to check the next character.
             if (isDecimalDigit(source[index + 1])) {
                 return scanNumericLiteral();
             }
