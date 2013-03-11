8c05a340102c005910885dc749a996b3fb018761
Ariya Hidayat <ariya.hidayat@gmail.com>
2013-03-10 19:12:06 -0700

    Source rewriter demo: use Eclipse Orion as the editor.
    https://code.google.com/p/esprima/issues/detail?id=402

--- demo/rewrite.js
+++ demo/rewrite.js
@@ -23,7 +23,7 @@
 */
 
 /*jslint browser:true */
-/*global esprima:true, escodegen:true */
+/*global esprima:true, escodegen:true, require:true */
 
 function id(i) {
     'use strict';
@@ -47,10 +47,8 @@ function sourceRewrite() {
 
     setText('error', '');
     if (typeof window.editor !== 'undefined') {
-        // Using CodeMirror.
-        code = window.editor.getValue();
+        code = window.editor.getText();
     } else {
-        // Plain textarea, likely in a situation where CodeMirror does not work.
         code = id('code').value;
     }
 
@@ -88,7 +86,7 @@ function sourceRewrite() {
         setText('error', e.toString());
     } finally {
         if (typeof window.editor !== 'undefined') {
-            window.editor.setValue(code);
+            window.editor.setText(code);
         } else {
             id('code').value = code;
         }
@@ -96,7 +94,7 @@ function sourceRewrite() {
 }
 
 /*jslint sloppy:true browser:true */
-/*global sourceRewrite:true, CodeMirror:true */
+/*global sourceRewrite:true */
 window.onload = function () {
     var version, el;
 
@@ -113,19 +111,9 @@ window.onload = function () {
     id('rewrite').onclick = sourceRewrite;
 
     try {
-        window.checkEnv();
-
-        // This is just testing, to detect whether CodeMirror would fail or not
-        window.editor = CodeMirror.fromTextArea(id("test"));
-
-        window.editor = CodeMirror.fromTextArea(id("code"), {
-            lineNumbers: true,
-            matchBrackets: true
+        require(['orion/editor/edit'], function (editor) {
+            window.editor = editor({ parent: 'editor', lang: 'js' });
         });
     } catch (e) {
-        // CodeMirror failed to initialize, possible in e.g. old IE.
-        id('codemirror').innerHTML = '';
-    } finally {
-        id('testbox').parentNode.removeChild(id('testbox'));
     }
 };
