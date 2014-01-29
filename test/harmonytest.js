f5a68a6e951bf008da504b6fbfb84d856313846c
Marshall Roch <mroch@fb.com>
2014-01-29 12:19:40 -0800

    Add more unit tests for arrow functions
    https://code.google.com/p/esprima/issues/detail?id=488

--- test/harmonytest.js
+++ test/harmonytest.js
@@ -1102,6 +1102,150 @@ var harmonyTestFixture = {
             }
         },
 
+        'e => ({ property: 42 })': {
+            type: 'ExpressionStatement',
+            expression: {
+                type: 'ArrowFunctionExpression',
+                id: null,
+                params: [{
+                    type: 'Identifier',
+                    name: 'e',
+                    range: [0, 1],
+                    loc: {
+                        start: { line: 1, column: 0 },
+                        end: { line: 1, column: 1 }
+                    }
+                }],
+                defaults: [],
+                body: {
+                    type: 'ObjectExpression',
+                    properties: [{
+                        type: 'Property',
+                        key: {
+                            type: 'Identifier',
+                            name: 'property',
+                            range: [8, 16],
+                            loc: {
+                                start: { line: 1, column: 8 },
+                                end: { line: 1, column: 16 }
+                            }
+                        },
+                        value: {
+                            type: 'Literal',
+                            value: 42,
+                            raw: '42',
+                            range: [18, 20],
+                            loc: {
+                                start: { line: 1, column: 18 },
+                                end: { line: 1, column: 20 }
+                            }
+                        },
+                        kind: 'init',
+                        method: false,
+                        shorthand: false,
+                        range: [8, 20],
+                        loc: {
+                            start: { line: 1, column: 8 },
+                            end: { line: 1, column: 20 }
+                        }
+                    }],
+                    range: [6, 22],
+                    loc: {
+                        start: { line: 1, column: 6 },
+                        end: { line: 1, column: 22 }
+                    }
+                },
+                rest: null,
+                generator: false,
+                expression: true,
+                range: [0, 23],
+                loc: {
+                    start: { line: 1, column: 0 },
+                    end: { line: 1, column: 23 }
+                }
+            },
+            range: [0, 23],
+            loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 1, column: 23 }
+            }
+        },
+
+        // Not an object!
+        'e => { label: 42 }': {
+            type: 'ExpressionStatement',
+            expression: {
+                type: 'ArrowFunctionExpression',
+                id: null,
+                params: [{
+                    type: 'Identifier',
+                    name: 'e',
+                    range: [0, 1],
+                    loc: {
+                        start: { line: 1, column: 0 },
+                        end: { line: 1, column: 1 }
+                    }
+                }],
+                defaults: [],
+                body: {
+                    type: 'BlockStatement',
+                    body: [{
+                        type: 'LabeledStatement',
+                        label: {
+                            type: 'Identifier',
+                            name: 'label',
+                            range: [7, 12],
+                            loc: {
+                                start: { line: 1, column: 7 },
+                                end: { line: 1, column: 12 }
+                            }
+                        },
+                        body: {
+                            type: 'ExpressionStatement',
+                            expression: {
+                                type: 'Literal',
+                                value: 42,
+                                raw: '42',
+                                range: [14, 16],
+                                loc: {
+                                    start: { line: 1, column: 14 },
+                                    end: { line: 1, column: 16 }
+                                }
+                            },
+                            range: [14, 17],
+                            loc: {
+                                start: { line: 1, column: 14 },
+                                end: { line: 1, column: 17 }
+                            }
+                        },
+                        range: [7, 17],
+                        loc: {
+                            start: { line: 1, column: 7 },
+                            end: { line: 1, column: 17 }
+                        }
+                    }],
+                    range: [5, 18],
+                    loc: {
+                        start: { line: 1, column: 5 },
+                        end: { line: 1, column: 18 }
+                    }
+                },
+                rest: null,
+                generator: false,
+                expression: false,
+                range: [0, 18],
+                loc: {
+                    start: { line: 1, column: 0 },
+                    end: { line: 1, column: 18 }
+                }
+            },
+            range: [0, 18],
+            loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 1, column: 18 }
+            }
+        },
+
         '(a, b) => { 42; }': {
             type: 'ExpressionStatement',
             expression: {
