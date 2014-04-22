e901bf7be3b3454b2b5c3f1acac0e7266da03304
Ariya Hidayat <ariya.hidayat@gmail.com>
2014-04-21 21:43:02 -0700

    Fast path in advance() should check for semicolon instead of colon.
    https://code.google.com/p/esprima/issues/detail?id=534

--- esprima.js
+++ esprima.js
@@ -1338,7 +1338,7 @@ parseStatement: true, parseSourceElement: true */
         }
 
         // Very common: ( and ) and ;
-        if (ch === 0x28 || ch === 0x29 || ch === 0x3A) {
+        if (ch === 0x28 || ch === 0x29 || ch === 0x3B) {
             return scanPunctuator();
         }
 
