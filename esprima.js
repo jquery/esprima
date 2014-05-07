d4caf49134c647c7b3c985fa53203c944bfb1d71
mrennie <Michael_Rennie@ca.ibm.com>
2014-05-07 11:33:07 -0400

    Issue 538:	When tolerant:true a missing comma should recover object properties
    https://code.google.com/p/esprima/issues/detail?id=538

--- esprima.js
+++ esprima.js
@@ -2159,7 +2159,7 @@ parseStatement: true, parseSourceElement: true */
     }
 
     function parseObjectInitialiser() {
-        var properties = [], property, name, key, kind, map = {}, toString = String, node = new Node();
+        var properties = [], token, property, name, key, kind, map = {}, toString = String, node = new Node();
 
         expect('{');
 
@@ -2196,7 +2196,16 @@ parseStatement: true, parseSourceElement: true */
             properties.push(property);
 
             if (!match('}')) {
-                expect(',');
+                if (extra.errors) {
+                    token = lookahead;
+                    if (token.type !== Token.Punctuator && token.value !== ',') {
+                        throwErrorTolerant(token, Messages.UnexpectedToken, token.value);
+                    } else {
+                        lex();
+                    }
+                } else {
+                    expect(',');
+                }
             }
         }
 
