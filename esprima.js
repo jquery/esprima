f951699cfd3180f7c95de1039010995168d30ac5
Ariya Hidayat <ariya.hidayat@gmail.com>
2015-02-18 23:09:21 -0800

    Support for ES6 lexical declaration in switch statement.
    Refs #1065.

--- esprima.js
+++ esprima.js
@@ -3420,7 +3420,7 @@
             if (match('}') || matchKeyword('default') || matchKeyword('case')) {
                 break;
             }
-            statement = parseStatement();
+            statement = parseSourceElement();
             consequent.push(statement);
         }
 
