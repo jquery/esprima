469accd7726d500b555e06ffd678c38ffb636750
Ariya Hidayat <ariya.hidayat@gmail.com>
2015-06-30 04:55:04 -0700

    Downstream project test: include esmangle and JSCS.
    Refs #1023
    Closes gh-1217

--- test/downstream.js
+++ test/downstream.js
@@ -88,7 +88,9 @@ function test_downstream(projects) {
 
 test_downstream({
     'escope': 'https://github.com/estools/escope.git',
+    'esmangle': 'https://github.com/estools/esmangle.git',
     'escomplex-js': 'https://github.com/philbooth/escomplex-js.git',
     'js2coffee': 'https://github.com/js2coffee/js2coffee.git',
+    'jscs': 'https://github.com/jscs-dev/node-jscs.git',
     'istanbul': 'https://github.com/gotwarlost/istanbul.git'
 });
