e1e6c280acc06858fc5bd97dfc23b184ecd26838
Jason Laster <jason.laster.11@gmail.com>
2015-07-16 14:20:50 -0400

    istanbul

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,6 @@ module.exports = function(config) {
     frameworks: ['mocha', 'chai', 'leche'],
 
     files: [
-      'esprima.js',
       'test/unit-tests.js',
     ],
 
@@ -18,14 +17,31 @@ module.exports = function(config) {
     },
 
     preprocessors: {
-        'test/unit-tests.js': ['webpack', 'sourcemap'],
+        'test/unit-tests.js': ['webpack', 'sourcemap', 'coverage'],
     },
 
     webpack: {
-        devtool: '#inline-source-map'
+       module: {
+           loaders: [],
+           postLoaders: [ { // << add subject as webpack's postloader
+               test: /\.js$/,
+               exclude: /(test|node_modules|bower_components)\//,
+               loader: 'istanbul-instrumenter'
+           } ]
+       },
+      devtool: '#inline-source-map'
     },
 
-    reporters: ['dots'], // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    coverageReporter: {
+     // specify a common output directory
+     dir: 'coverage',
+     reporters: [
+       // reporters not supporting the `file` property
+       { type: 'json', subdir: 'report-json' },
+     ]
+   },
+
+    reporters: ['dots', 'coverage'], // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     port: 9876,
     colors: true,
     logLevel: config.LOG_WARN, // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
