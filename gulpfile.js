705ff4265720e230ee114bb4dd8e391334e06b49
Jason Laster <jason.laster.11@gmail.com>
2015-07-16 12:26:08 -0400

    cleanup gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,58 +3,10 @@ var gulp = require('gulp'),
     map = require('map-stream'),
     watch = require('gulp-watch'),
     File = require('vinyl'),
-    gulpSequence = require('gulp-sequence'),
     karma = require('karma'),
     _ = require('lodash'),
     Mocha = require('mocha'),
-    fs = require('fs'),
-    path = require('path');
-
-
-var writeFixtures = function(ext) {
-  return function(file, cb) {
-
-   var content = file.contents.toString();
-   var filePath = file.path.substring(file.base.length, file.path.length - ext.length -1);
-
-
-   var newContent = "__fixtures_"+ext+"__['"+filePath+"'] = " + JSON.stringify(content) + ";\n\n";
-
-   var newFile = new File({
-     path: file.path,
-     contents: new Buffer(newContent)
-   });
-
-   cb(null, newFile);
- };
-}
-
-var templateFixturesFile = function(ext) {
-  return function(file, cb) {
-     var content = file.contents.toString();
-
-     var newContent = "";
-     newContent += "var __fixtures_"+ext+"__ = {};\n";
-     newContent += content;
-     newContent += "module.exports = __fixtures_"+ext+"__";
-
-     var newFile = new File({
-       path: file.path,
-       contents: new Buffer(newContent)
-     });
-    cb(null, newFile)
-  }
-}
-
-function buildFixture(ext) {
-  return function() {
-    gulp.src(['test/fixtures/**/*.'+ext])
-      .pipe(map(writeFixtures(ext)))
-      .pipe(concat('fixtures_'+ext+'.js'))
-      .pipe(map(templateFixturesFile(ext)))
-      .pipe(gulp.dest('test/dist/'));
-  }
-}
+    fs = require('fs');
 
 function karmaStart(browser, options) {
   karma.server.start(_.extend({
@@ -63,66 +15,83 @@ function karmaStart(browser, options) {
   }, options));
 }
 
+function mocha(done) {
+  var mocha = new Mocha({
+    ui: 'bdd',
+    growl: true,
+    useColors: true,
+    useInlineDiffs: true
+  });
 
-gulp.task('jsFixtures', function() {
-  return buildFixture('js')();
+  mocha.addFile('test/unit-tests.js');
+  return mocha.run(done);
+}
+
+function buildFixture(ext) {
+    var writeFixtures = function(ext) {
+      return function(file, cb) {
+
+       var content = file.contents.toString();
+       var filePath = file.path.substring(file.base.length, file.path.length - ext.length -1);
+
+       var newContent = "__fixtures_"+ext+"__['"+filePath+"'] = " + JSON.stringify(content) + ";\n\n";
+
+       var newFile = new File({
+         path: file.path,
+         contents: new Buffer(newContent)
+       });
+
+       cb(null, newFile);
+     };
+    }
+
+    var templateFixturesFile = function(ext) {
+      return function(file, cb) {
+         var content = file.contents.toString();
+
+         var newContent = "";
+         newContent += "var __fixtures_"+ext+"__ = {};\n";
+         newContent += content;
+         newContent += "module.exports = __fixtures_"+ext+"__";
+
+         var newFile = new File({
+           path: file.path,
+           contents: new Buffer(newContent)
+         });
+        cb(null, newFile)
+      }
+    }
+
+  return function() {
+    gulp.dest('test/dist/');
+    return gulp.src(['test/fixtures/**/*.'+ext])
+      .pipe(map(writeFixtures(ext)))
+      .pipe(concat('fixtures_'+ext+'.js'))
+      .pipe(map(templateFixturesFile(ext)))
+      .pipe(gulp.dest('test/dist/'));
+  }
+}
+
+gulp.task('buildFixtures', function() {
+  buildFixture('js')();
+  buildFixture('json')();
 });
 
-gulp.task('jsonFixtures', function() {
-  return buildFixture('json')();
+gulp.task('watchFixtures', function() {
+  gulp.watch('test/fixtures/**/**', ['buildFixtures']);
 });
 
-gulp.task('watchJsFixtures', function () {
-  return watch('test/fixtures/**/*.js', buildFixture('js'));
-})
-
-gulp.task('watchJsonFixtures', function() {
-  return watch('test/fixtures/**/*.json', buildFixture('json'));
-})
-
-gulp.task('karmaSingleRun', function () {
-  karmaStart('PhantomJS', {singleRun: true});
+gulp.task('test', ['buildFixtures'], function() {
+  return gulp.src('test/unit-tests/*.js')
+    .pipe(mocha(function(errCount) {
+      if (errCount) {
+        console.log(errCount + ' failed tests.');
+        process.exit(1);
+      }
+      process.exit(0);
+    }));
 });
 
-gulp.task('karmaChrome', function () {
+gulp.task('test-chrome', ['watchFixtures'], function() {
   karmaStart('Chrome');
 });
-
-gulp.task('karmaPhantom', function () {
-  karmaStart('Phantom');
-});
-
-gulp.task('nodeMocha', function(done) {
-  var mocha = new Mocha();
-
-  mocha.addFile('test/unit-tests/api.js');
-  mocha.addFile('test/unit-tests/failure.js');
-  mocha.addFile('test/unit-tests/module.js');
-  mocha.addFile('test/unit-tests/tokens.js');
-  mocha.addFile('test/unit-tests/tree.js');
-
-  mocha.run(function(failures){
-    done();
-  });
-})
-
-gulp.task('test',
-  gulpSequence(
-    ['jsFixtures', 'jsonFixtures'],
-    'nodeMocha'
-  )
-);
-
-gulp.task('test-watch',
-  gulpSequence(
-    ['jsFixtures', 'jsonFixtures', 'karmaPhantom'],
-    ['watchJsFixtures', 'watchJsonFixtures']
-  )
-);
-
-gulp.task('test-chrome',
-  gulpSequence(
-    ['jsFixtures', 'jsonFixtures', 'karmaChrome'],
-    ['watchJsFixtures', 'watchJsonFixtures']
-  )
-);
