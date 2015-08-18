883be7b56444538d190138dbd761b3acc16ad960
Ariya Hidayat <ariya.hidayat@gmail.com>
2015-08-17 20:50:56 -0700

    Downstream project test: include webpack.
    Refs #1023.

--- test/downstream.js
+++ test/downstream.js
@@ -92,5 +92,6 @@ test_downstream({
     'escomplex-js': 'https://github.com/philbooth/escomplex-js.git',
     'js2coffee': 'https://github.com/js2coffee/js2coffee.git',
     'jscs': 'https://github.com/jscs-dev/node-jscs.git',
-    'istanbul': 'https://github.com/gotwarlost/istanbul.git'
+    'istanbul': 'https://github.com/gotwarlost/istanbul.git',
+    'webpack': 'https://github.com/webpack/webpack.git'
 });
