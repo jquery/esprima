83d77984c7bba79d25e7b1389c432133879fa1a6
Ariya Hidayat <ariya.hidayat@gmail.com>
2015-09-14 19:33:44 -0700

    Use Sauce Labs to run the test suite.
    Since the FOSS plan of Sauce Labs only allows 5 concurrent browsers, the
    tests are splitted to 3 runs with a different configuration.
    
    Fixes #1259
    Closes gh-1326

---
+++ test/saucelabs-safari.conf.js
@@ -0,0 +1,32 @@
+var configureSauceLabs = require('./configure-sauce-labs.js');
+
+module.exports = function (config) {
+
+    configureSauceLabs(config, 'Safari browsers', {
+        Safari_8: {
+            base: 'SauceLabs',
+            browserName: 'safari',
+            platform: 'OS X 10.10',
+            version: '8.0'
+        },
+        Safari_7: {
+            base: 'SauceLabs',
+            browserName: 'safari',
+            platform: 'OS X 10.9',
+            version: '7.0'
+        },
+        MobileSafari_9: {
+            base: 'SauceLabs',
+            browserName: 'iphone',
+            platform: 'OS X 10.10',
+            version: '9.0'
+        },
+        MobileSafari_8: {
+            base: 'SauceLabs',
+            browserName: 'iphone',
+            platform: 'OS X 10.10',
+            version: '8.4'
+        }
+    });
+
+}
