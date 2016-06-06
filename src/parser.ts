54213ad4e696ca0949956125582d3eb70e4e808f
fishbar <zhengxinlin@gmail.com>
2016-06-06 17:25:21 +0800

    deep clone node when node.shorthand = true
    Fix #1755
    Closes gh-1756

--- src/parser.ts
+++ src/parser.ts
@@ -902,7 +902,7 @@ export class Parser {
                     value = this.finalize(node, new Node.AssignmentPattern(id, init));
                 } else {
                     shorthand = true;
-                    value = id;
+                    value = this.finalize(node, new Node.Identifier(token.value));
                 }
             } else {
                 this.throwUnexpectedToken(this.nextToken());
@@ -1938,7 +1938,7 @@ export class Parser {
             } else if (!this.match(':')) {
                 params.push(keyToken);
                 shorthand = true;
-                value = init;
+                value = this.finalize(node, new Node.Identifier(keyToken.value));
             } else {
                 this.expect(':');
                 value = this.parsePatternWithDefault(params, kind);
