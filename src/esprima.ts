15fec5c1435443632289ec3ff80c599092a84607
Ariya Hidayat <ariya.hidayat@gmail.com>
2016-09-13 08:33:39 -0700

    Do not implicitly collect comments when comment attachment is specified
    Fixes #1553
    Closes gh-1560

--- src/esprima.ts
+++ src/esprima.ts
@@ -39,8 +39,9 @@ export function parse(code, options, delegate) {
     };
 
     let parserDelegate = (typeof delegate === 'function') ? proxyDelegate : null;
+    let collectComment = false;
     if (options) {
-        const collectComment = (typeof options.comment === 'boolean' && options.comment);
+        collectComment = (typeof options.comment === 'boolean' && options.comment);
         const attachComment = (typeof options.attachComment === 'boolean' && options.attachComment);
         if (collectComment || attachComment) {
             commentHandler = new CommentHandler();
@@ -59,7 +60,7 @@ export function parse(code, options, delegate) {
 
     const ast = <any>(parser.parseProgram());
 
-    if (parser.config.comment) {
+    if (collectComment) {
         ast.comments = commentHandler.comments;
     }
     if (parser.config.tokens) {
