d5f975e1806e62349ac3f236f9c81e5bf0bf9ffa
Ariya Hidayat <ariya.hidayat@gmail.com>
2016-12-12 20:02:35 -0800

    Only permit hex digits in hex escape sequence
    Adjust the error message to follow V8.
    Also make it explicit that scanHexEscape can return null (upon an
    invalid sequence).
    
    Fixes #1619

--- src/messages.ts
+++ src/messages.ts
@@ -14,6 +14,7 @@ export const Messages = {
     IllegalExportDeclaration: 'Unexpected token',
     IllegalImportDeclaration: 'Unexpected token',
     IllegalReturn: 'Illegal return statement',
+    InvalidHexEscapeSequence: 'Invalid hexadecimal escape sequence',
     InvalidLHSInAssignment: 'Invalid left-hand side in assignment',
     InvalidLHSInForIn: 'Invalid left-hand side in for-in',
     InvalidLHSInForLoop: 'Invalid left-hand side in for-loop',
