000a0f91277385b42e88fc121ed0354fc5284c25
Matheus Dias de Souza <Handonwhitecloud@outlook.com>
2017-04-10 20:56:47 -0300

    More conditional Zs check
    Sorry for editing this from Github's editor, but the repository is heavy to easily fork.

--- src/character.ts
+++ src/character.ts
@@ -21,13 +21,14 @@ export const Character = {
 
     isWhiteSpace(cp: number): boolean {
         return (cp === 0x20) || (cp === 0x09) || (cp === 0x0B) || (cp === 0x0C) || (cp === 0xA0) ||
-            (cp >= 0x1680 && [0x1680, 0x2000, 0x2001, 0x2002, 0x2003, 0x2004, 0x2005, 0x2006, 0x2007, 0x2008, 0x2009, 0x200A, 0x202F, 0x205F, 0x3000, 0xFEFF].indexOf(cp) >= 0);
+            (cp >= 0x1680 && ((cp === 0x1680) || ((cp >= 0x2000) && (cp <= 0x200A)) ||
+                ([0x202F, 0x205F, 0x3000, 0xFEFF].indexOf(cp) >= 0)));
     },
 
     // https://tc39.github.io/ecma262/#sec-line-terminators
 
     isLineTerminator(cp: number): boolean {
-        return (cp === 0x0A) || (cp === 0x0D) || (cp === 0x2028) || (cp === 0x2029);
+        return (cp === 0x0A) || (cp === 0x0D) || ((cp >= 0x2028) && (cp <= 0x2029));
     },
 
     // https://tc39.github.io/ecma262/#sec-names-and-keywords
