474d6cd1ac826956089e7e61faefab885c5c9275
johnjbarton <johnjbarton@johnjbarton.com>
2017-11-17 09:32:50 -0800

    Recognize Token.Template as a ReturnStatement argument.
    When a Template is broken across multiple lines, the existing hasArgument test fails.
    The Template is scanned as a single token so we just need to check for it after the return.
    
    Fixes #1829
    Closes gh-1882

--- src/parser.ts
+++ src/parser.ts
@@ -2462,7 +2462,6 @@ export class Parser {
 
         const hasArgument = (!this.match(';') && !this.match('}') &&
             !this.hasLineTerminator && this.lookahead.type !== Token.EOF) ||
-            this.lookahead.type === Token.StringLiteral ||
             this.lookahead.type === Token.Template;
 
         const argument = hasArgument ? this.parseExpression() : null;
