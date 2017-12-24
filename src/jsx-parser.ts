ffcb3de6d3c1bb696601e4533e7283ab7542eafd
Ariya Hidayat <ariya.hidayat@gmail.com>
2017-12-23 20:30:55 -0800

    Update TypeScript compiler, linter, and formatter to the latest version
    Some formatting tweaks and identifier renaming (to adhere to the
    variable unshadowing rule) are necessary.

--- src/jsx-parser.ts
+++ src/jsx-parser.ts
@@ -484,9 +484,9 @@ export class JSXParser extends Parser {
         this.expectJSX('<');
         if (this.matchJSX('/')) {
             this.expectJSX('/');
-            const name = this.parseJSXElementName();
+            const elementName = this.parseJSXElementName();
             this.expectJSX('>');
-            return this.finalize(node, new JSXNode.JSXClosingElement(name));
+            return this.finalize(node, new JSXNode.JSXClosingElement(elementName));
         }
 
         const name = this.parseJSXElementName();
