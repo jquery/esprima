e9f466a1e941737933c048308fa2b14321aa7438
Ariya Hidayat <ariya.hidayat@gmail.com>
2018-05-19 15:04:58 -0700

    Downstream test: ignore Recast since its master is flaky.
    Recast's TypeScript tests often fail spuriously, sometimes in conjuction with GraphQL,
    so let's isolate us from it.
    
    Closes gh-1931

--- test/downstream.js
+++ test/downstream.js
@@ -130,7 +130,7 @@ var projects = {
     'redeyed': 'https://github.com/thlorenz/redeyed.git',
     'jsfmt': 'https://github.com/rdio/jsfmt.git',
     // 'assetgraph': 'https://github.com/assetgraph/assetgraph.git',
-    'recast': 'https://github.com/benjamn/recast.git',
+    // 'recast': 'https://github.com/benjamn/recast.git',
     'rocambole': 'https://github.com/millermedeiros/rocambole.git'
     // 'documentjs': 'https://github.com/bitovi/documentjs.git'
     // 'istanbul': 'https://github.com/gotwarlost/istanbul.git'
