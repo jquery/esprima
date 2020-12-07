1f3fa44c1519d3707ea23b5bbc614b17dc8c38ff
Remi Liu <ljqx@live.cn>
2020-12-07 15:14:42 +0800

    infrastructure: update "engines" to node>=10.4

--- src/scanner.ts
+++ src/scanner.ts
@@ -527,7 +527,7 @@ export class Scanner {
 
             // 3 digits are only allowed when string starts
             // with 0, 1, 2, 3
-            if ('0123'.indexOf(ch) >= 0 && !this.eof() && Character.isOctalDigit(this.source.charCodeAt(this.index))) {
+            if ('0123'.includes(ch) && !this.eof() && Character.isOctalDigit(this.source.charCodeAt(this.index))) {
                 code = code * 8 + octalValue(this.source[this.index++]);
             }
         }
@@ -661,7 +661,7 @@ export class Scanner {
 
                             // 1-character punctuators.
                             str = this.source[this.index];
-                            if ('<>=!+-*%&|^/'.indexOf(str) >= 0) {
+                            if ('<>=!+-*%&|^/'.includes(str)) {
                                 ++this.index;
                             }
                         }
@@ -1136,7 +1136,7 @@ export class Scanner {
         const astralSubstitute = '\uFFFF';
         let tmp = pattern;
 
-        if (flags.indexOf('u') >= 0) {
+        if (flags.includes('u')) {
             tmp = tmp
                 // Replace every Unicode escape sequence with the equivalent
                 // BMP character or a constant ASCII code point in the case of
